e931fb0eb90f48d441084c81541ee274994b1518
Tris Setiawan <tris.setiawan@yahoo.com>
2017-10-11 16:59:56 +0700

    Default model implementations

---
+++ StandardEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Moonlay.Models
+{
+    public abstract class StandardEntity : BaseEntity, IActive, IAuditEntity, ISoftEntity
+    {  
+        public bool Active { get; set; }
+
+        public DateTime _CreatedUtc { get; set; }
+        public string _CreatedBy { get; set; }
+        public string _CreatedAgent { get; set; }
+
+        public DateTime _LastModifiedUtc { get; set; }
+        public string _LastModifiedBy { get; set; }
+        public string _LastModifiedAgent { get; set; }
+
+        public bool _IsDeleted { get; set; } 
+        public DateTime _DeletedUtc { get; set; } 
+        public string _DeletedBy { get; set; } 
+        public string _DeletedAgent { get; set; }
+    }
+
+    public abstract class StandardEntity<TKey> : StandardEntity, IEntity<TKey>
+    {
+        public new TKey Id { get; set; }
+    }
+}
